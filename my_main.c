3fe78086194d1ad22835e7e1ff97591eca4d8c5c
djiang2001 <djiang2@stuy.edu>
2019-05-15 22:36:04 -0400

    done

--- my_main.c
+++ my_main.c
@@ -53,11 +53,13 @@ void my_main() {
 
   int i;
   struct matrix *tmp;
+  struct matrix *edges;
+  struct matrix *polygons;
   struct stack *systems;
   screen t;
   zbuffer zb;
   color g;
-  double step_3d = 20;
+  double step_3d = 200;
   double theta;
 
   //Lighting values here for easy access
@@ -71,7 +73,7 @@ void my_main() {
   light[LOCATION][1] = 0.75;
   light[LOCATION][2] = 1;
 
-  light[COLOR][RED] = 0;
+  light[COLOR][RED] = 255;
   light[COLOR][GREEN] = 255;
   light[COLOR][BLUE] = 255;
 
@@ -97,9 +99,11 @@ void my_main() {
   //constants are a pointer in symtab, using one here for consistency
   struct constants *reflect;
   reflect = &white;
-
+  
   systems = new_stack();
   tmp = new_matrix(4, 1000);
+  polygons = new_matrix(4,4);
+  edges = new_matrix(4,4);
   clear_screen( t );
   clear_zbuffer(zb);
   g.red = 0;
@@ -110,7 +114,133 @@ void my_main() {
   for (i=0;i<lastop;i++) {
 
     printf("%d: ",i);
-
+    switch(op[i].opcode){
+      //PUSH
+    case PUSH:
+      printf("PUSH");
+      push(systems);
+      break;
+      //POP
+    case POP:
+      printf("POP");
+      pop(systems);
+      break;
+      //MOVE
+    case MOVE:
+      printf("MOVE");
+      tmp = make_translate(op[i].op.move.d[0],
+			   op[i].op.move.d[1],
+			   op[i].op.move.d[2]);
+      matrix_mult(peek(systems),tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //ROTATE
+    case ROTATE:
+      printf("ROTATE");
+      theta = op[i].op.rotate.degrees * (M_PI / 180);
+      if(op[i].op.rotate.axis == 0.0){
+	tmp = make_rotX(theta);
+      }
+      else if(op[i].op.rotate.axis == 1.0){
+	tmp = make_rotY(theta);
+      }
+      else{
+	tmp = make_rotZ(theta);
+      }
+      matrix_mult(peek(systems), tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //SCALE
+    case SCALE:
+      printf("SCALE");
+      tmp = make_scale(op[i].op.scale.d[0],
+		       op[i].op.scale.d[1],
+		       op[i].op.scale.d[2]);
+      matrix_mult(peek(systems),tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //BOX
+    case BOX:
+      printf("BOX");
+      add_box(polygons,
+	      op[i].op.box.d0[0],
+	      op[i].op.box.d0[1],
+	      op[i].op.box.d0[2],
+	      op[i].op.box.d1[0],
+	      op[i].op.box.d1[1],
+	      op[i].op.box.d1[2]);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if(op[i].op.box.constants != NULL){
+	reflect = op[i].op.box.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //SPHERE
+    case SPHERE:
+      printf("SPHERE");
+      add_sphere(polygons,
+		 op[i].op.sphere.d[0],
+		 op[i].op.sphere.d[1],
+		 op[i].op.sphere.d[2],
+		 op[i].op.sphere.r,
+		 step_3d);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if(op[i].op.sphere.constants != NULL){
+	reflect = op[i].op.sphere.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //TORUS
+    case TORUS:
+      printf("TORUS");
+      add_torus(polygons,
+		op[i].op.torus.d[0],
+		op[i].op.torus.d[1],
+		op[i].op.torus.d[2],
+		op[i].op.torus.r0,
+		op[i].op.torus.r1,
+		step_3d);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if (op[i].op.torus.constants != NULL){
+	reflect = op[i].op.torus.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //CONSTANTS
+    case CONSTANTS:
+      printf("CONSTANTS");
+      break;
+      //LINE
+    case LINE:
+      printf("LINE");
+      add_edge(edges,
+	       op[i].op.line.p0[0],
+	       op[i].op.line.p0[1],
+	       op[i].op.line.p0[2],
+	       op[i].op.line.p1[0],
+	       op[i].op.line.p1[1],
+	       op[i].op.line.p1[2]);
+      matrix_mult(peek(systems),edges);
+      draw_lines(edges, t, zb, g);
+      edges->lastcol = 0;
+      break;
+      //SAVE
+    case SAVE:
+      printf("SAVE");
+      save_extension(t, op[i].op.save.p->name);
+      break;
+      //DISPLAY
+    case DISPLAY:
+      printf("DISPLAY");
+      display(t);
+      break;
+    }
     printf("\n");
   }
 }
